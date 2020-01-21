50818737f15b35c2c12e2a1012c91ce8ad534b2c
levidevv <60077045+levidevv@users.noreply.github.com>
2020-01-20 19:08:18 -0600

    Add files via upload

---
+++ EjercicioDos.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class EjercicioDos{
+
+   public static void main(String args[]){
+    Scanner in = new Scanner(System.in);
+    String nombre="";
+    int numUno = 0, numDos = 0, resultado = 0, operacion = 0;
+
+    System.out.println("Hola, ¿cuál es tu nombre? ");
+    nombre = in.nextLine();
+    System.out.println("Mucho gusto " + nombre);
+
+    System.out.println("Selecciona la operación que necesitas por favor.");
+    System.out.println("Suma = 1, Resta = 2, Multiplica = 3, Divide = 4");
+    operacion = in.nextInt();
+    if(operacion >=5){System.out.println("No es opcion valida por favor ingresa la operacion que deseas realizar");}
+    System.out.println("Suma = 1, Resta = 2, Multiplica = 3, Divide = 4");
+    operacion = in.nextInt();
+
+    System.out.println("Tu primer número por favor: ");
+    numUno = in.nextInt();
+
+    System.out.println("Tu segundo número por favor: ");
+    numDos = in.nextInt();
+
+    if(operacion == 1){
+      resultado = numUno + numDos;
+      System.out.println("El resultado de tu suma es: " + resultado);
+    }else if(operacion ==2){
+      resultado = numUno - numDos;
+      System.out.println("El resultado de tu resta es; " + resultado);
+     }else if(operacion == 3){
+       resultado = numUno * numDos;
+       System.out.println("El resultado de tu operación es: " + resultado);
+      }else if(operacion == 4){
+        resultado = numUno / numDos;
+        System.out.println("El resultado de tu operación es: " + resultado);
+    }else{
+         System.out.println("No es opción valida");
+        }
+
+    System.out.println("Gracias " + nombre); 
+
+  }
+}
